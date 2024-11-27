bd4b493d327d6aff3751beb8a25c0d85a48982fa
Sehjan <sehjankhan882@gmail.com>
2024-11-27 16:14:41 +0530

    created sliders

--- lib/src/Screens/Landing/landing_page.dart
+++ lib/src/Screens/Landing/landing_page.dart
@@ -1,3 +1,4 @@
+import 'package:aether_drive_flutter/src/Screens/Slider%20Pages/slider_login_page.dart';
 import 'package:flutter/material.dart';
 
 class LandingPG extends StatefulWidget {
@@ -15,55 +16,75 @@ class _LandingPGState extends State<LandingPG> {
       home: Scaffold(
         backgroundColor: Color(0xff004ca8),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Image.asset(
-                    'assets/images/Group 10.png',
-                    scale: 3,
-                  ),
+                Image.asset(
+                  'assets/images/Group 10.png',
+                  scale: 3,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 0),
-                  child: Text(
-                    'ether',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'nunito',
-                        fontSize: 35),
-                  ),
+                Text(
+                  'ether',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'nunito',
+                      fontSize: 35),
                 ),
               ],
             ),
-            Container(height: MediaQuery.of(context).size.height * 0.2),
-            Image.asset('assets/images/rb_2942.png',scale: 7,),
-            Container(height: MediaQuery.of(context).size.height * 0.3),
-            Text(
-              'Welcome to Aether Drive Your personal cloud storage solution!',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'nunito',
-                  fontSize: 17),
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(38),
+                topRight: Radius.circular(38), // Adjust the radius as needed
+              ),
+              child: Container(
+                color: const Color(0xffffffff),
+                width: MediaQuery.of(context).size.width * 1,
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Welcome to Aether Drive Your personal cloud storage solution!',
+                      style: TextStyle(
+                          color: Color(0xff004ca8),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'nunito',
+                          fontSize: 17),
+                    ),
+                    Container(
+                        height: MediaQuery.of(context).size.height * 0.04),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width*0.3,
+                      height: MediaQuery.of(context).size.height*0.05,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: WidgetStatePropertyAll(
+                                Color(0xff005ac4),
+                              ),
+                              elevation: WidgetStatePropertyAll(20),
+                              shadowColor: WidgetStatePropertyAll(Colors.black)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SliderPG()));
+                          },
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                                color: Color(0xffffffff),
+                                fontFamily: 'nunito',
+                                fontSize: 20),
+                          )),
+                    )
+                  ],
+                ),
+              ),
             ),
-            Container(height: MediaQuery.of(context).size.height * 0.04),
-            ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Color(0xffFFFFFF)),
-                    elevation: WidgetStatePropertyAll(20),
-                    shadowColor: WidgetStatePropertyAll(Colors.black)),
-                onPressed: () {},
-                child: Text(
-                  'Get Started',
-                  style: TextStyle(
-                      color: Color(0xff004ca8),
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'nunito',
-                      fontSize: 20),
-                ))
           ],
         ),
       ),
