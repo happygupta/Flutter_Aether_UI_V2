b15e8f6eb7c317a8054da66e2f6ac3397087cba1
Sehjan <sehjankhan882@gmail.com>
2024-11-29 18:03:50 +0530

    Updates

--- lib/src/Screens/Splash/splash_screen.dart
+++ lib/src/Screens/Splash/splash_screen.dart
@@ -29,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 1), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const LandingPG()),
